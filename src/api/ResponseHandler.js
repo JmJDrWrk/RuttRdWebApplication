69a8ff50e1c6cae2b8d4a6ba7c0ca1fa7abcf58f
Jaime <73396010+JmJDrWrk@users.noreply.github.com>
2023-06-16 14:41:41 +0200

    Update ResponseHandler.js

--- src/api/ResponseHandler.js
+++ src/api/ResponseHandler.js
@@ -9,16 +9,27 @@ export default class ResponseHandler {
 
     static async getNotificationModel(data) {
         console.log('data: ' + JSON.stringify(data))
+        if(this.isSuccess(data)){
+            console.log("success")
+            data.type = 'sucess'
+        }else{
+            data.type = 'error'
+        }
+
+        if(!data.message){
+            data.message = 'Unknown error'
+        }
+        if(!data.title){
+            data.title = data.message
+        }
+        
+        return data
     }
 
     static handle(response, onSuccess, onError) {
         console.log("RES: ", response)
-        if(this.isSuccess(response)){
-            console.log("success")
-            // this.getNotificationModel(response)
-        }
-        this.getNotificationModel(response)
-
+        const notification = this.getNotificationModel(response)
+        alert(notification.title)
     }
 }
 
