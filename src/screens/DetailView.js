05a10e5d9ab8b37fe30afb221075e1aec5658b07
Jaime <jaimeromangil@gmail.com>
2023-06-17 13:14:01 +0200

    resolved

--- src/screens/DetailView.js
+++ src/screens/DetailView.js
@@ -3,9 +3,7 @@ import styled, { css } from "styled-components";
 import Header from "../components/mui/Header";
 import Footer from "../components/mui/Footer";
 import EntypoIcon from "react-native-vector-icons/dist/Entypo";
-import MaterialButtonShare from "../components/MaterialButtonShare";
-import MaterialCardWithImageAndTitle1 from "../components/MaterialCardWithImageAndTitle1";
-import MaterialRightIconTextbox1 from "../components/MaterialRightIconTextbox1";
+
 
 function DetailView(props) {
   return (
