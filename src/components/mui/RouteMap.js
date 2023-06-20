2d0e45140b209435b4441cd856856353b14fc059
Jaime <73396010+JmJDrWrk@users.noreply.github.com>
2023-06-20 11:46:27 +0200

    resolved
    feature/backlog-2
    feature/backlog-18

--- src/components/mui/RouteMap.js
+++ src/components/mui/RouteMap.js
@@ -303,10 +303,9 @@ const RouteMap = ({ rutt, belongsToUser }) => {
         price: price
       }
     };
-    const id = (await new RuttApi().uploadRutt()).data.ruttId
-    // const id = (await ruttApi.uploadRutt(ruttFile)).ruttId
+    const {data, succeeded} = (await ruttApi.uploadRutt(ruttFile))
+    navigate('/RuttView/' + data.ruttId)
 
-    navigate('/RuttView/' + id)
   }
 
   function handlePublishRutt() {
