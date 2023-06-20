70b3edb692906fa26bfd7b6e19ece7a19e794842
Jaime <73396010+JmJDrWrk@users.noreply.github.com>
2023-06-20 10:51:41 +0200

    Se detecta un error de implementación de los cambios en la rama feature/backlog-18
    https://trello.com/c/GDyLmPsG

--- src/components/mui/RouteMap.js
+++ src/components/mui/RouteMap.js
@@ -303,10 +303,10 @@ const RouteMap = ({ rutt, belongsToUser }) => {
         price: price
       }
     };
+    const id = (await ruttApi.uploadRutt()).data.ruttId
+    // const id = (await ruttApi.uploadRutt(ruttFile)).ruttId
 
-    const id = (await ruttApi.uploadRutt(ruttFile)).ruttId
-
-    navigate('/Rutt/' + id)
+    navigate('/RuttView/' + id)
   }
 
   function handlePublishRutt() {
